6795b615c92a4ad44e47aee8c230f8557e9359ee
negi0109 <negi0109.seg@gmail.com>
2023-08-02 04:11:38 +0900

    #65 Step

---
+++ Assets/Negi0109/ColorGradientToTexture/Scripts/ColorFilters/Step.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Negi0109.ColorGradientToTexture.Filters {
+
+    [System.Serializable]
+    public class Step : ColorFilter {
+        public int step = 1;
+
+        public override float Evaluate(float v) => (int)(v / (1f / step)) * (1f / step);
+
+        public override bool Editor() => ColorFilterEditorUtils.IntField("step", ref step, v => Mathf.Max(v, 1));
+    }
+
+}
