7a83c6650006caf2d7ca842377aaf83f23edbdc8
negi0109 <negi0109.seg@gmail.com>
2023-08-02 04:10:38 +0900

    #65 noise

---
+++ Assets/Negi0109/ColorGradientToTexture/Scripts/ColorFilters/Noise.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Negi0109.ColorGradientToTexture.Filters {
+
+    [System.Serializable]
+    public class Noise : ColorFilter {
+
+        public float weight = 0.5f;
+        public int seed = UnityEngine.Random.Range(0, int.MaxValue);
+
+        public override void Evaluate(ref float[,] v)
+        {
+            var width = v.GetLength(0);
+            var height = v.GetLength(1);
+
+            var r = new System.Random(seed);
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    v[x, y] += weight * r.Next() / int.MaxValue;
+                }
+            }
+        }
+
+        public override bool Editor() {
+            EditorGUILayout.BeginVertical();
+            EditorGUIUtility.labelWidth = 60f;
+
+            var updated = Watch(
+                () => weight,
+                () => { weight = EditorGUILayout.Slider("weight", weight, 0f, 1f); }
+            );
+            updated |= Watch(
+                () => (int) seed,
+                () => { seed = EditorGUILayout.IntField("seed", seed); }
+            );
+
+            EditorGUIUtility.labelWidth = 0;
+            EditorGUILayout.EndVertical();
+
+            return updated;
+        }
+    }
+
+}
