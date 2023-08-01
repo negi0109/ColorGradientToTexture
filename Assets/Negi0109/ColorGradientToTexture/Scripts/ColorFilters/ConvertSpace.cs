38e2dfd568a4840d8c1d5adc36dfccb7cff8b4dc
negi0109 <negi0109.seg@gmail.com>
2023-08-02 04:10:48 +0900

    #65 ConvertSpace

---
+++ Assets/Negi0109/ColorGradientToTexture/Scripts/ColorFilters/ConvertSpace.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Negi0109.ColorGradientToTexture.Filters {
+
+    [System.Serializable]
+    public class ConvertSpace : ColorFilter {
+        public enum ConvertType {
+            GammaToLinear,
+            LinearToGamma
+        }
+
+        public ConvertType type;
+        public override float Evaluate(float v) {
+            switch(type) {
+                case ConvertType.GammaToLinear: return Mathf.GammaToLinearSpace(v);
+                case ConvertType.LinearToGamma: return Mathf.LinearToGammaSpace(v);
+                default: return v;
+            }
+        }
+
+        public override bool Editor()
+        {
+            return Watch(
+                () => (int) type,
+                () => { type = (ConvertType) EditorGUILayout.EnumPopup(type); }
+            );
+        }
+    }
+
+}
