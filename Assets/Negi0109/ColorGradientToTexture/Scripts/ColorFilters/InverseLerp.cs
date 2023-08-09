0c1a21a7d0172d96e4d10614f897dfbe36be9b8c
negi0109 <negi0109.seg@gmail.com>
2023-08-10 03:31:47 +0900

    #70 InverseLerp関数を追加
    Clamp01も可能

---
+++ Assets/Negi0109/ColorGradientToTexture/Scripts/ColorFilters/InverseLerp.cs
@@ -0,0 +1,54 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Negi0109.ColorGradientToTexture.Filters
+{
+
+    [System.Serializable]
+    public class InverseLerp : ColorFilter
+    {
+        public Number min = new Number() { type = Number.Type.Min };
+        public Number max = new Number() { type = Number.Type.Max };
+        public bool clamped = true;
+
+        public override void EvaluateAll(ref float[,] array)
+        {
+            Utils.ArraySegment2DBase<float> segment = new Utils.ArraySegment2D<float>(array);
+
+            var minValue = min.GetValue(segment);
+            var maxValue = max.GetValue(segment);
+
+            segment.GetAll().SetValues(
+                v => clamped ? Mathf.InverseLerp(minValue, maxValue, v) : (v - minValue) / (maxValue - minValue)
+            );
+        }
+
+        public override bool Editor()
+        {
+            EditorGUIUtility.labelWidth = 60f;
+
+            var updated = false;
+
+            EditorGUILayout.BeginVertical();
+
+            updated |= Watch(() => clamped, () => { clamped = EditorGUILayout.Toggle("Clamp01", clamped); });
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("min", new GUILayoutOption[] { GUILayout.Width(40) });
+
+            updated |= min.Editor();
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("max", new GUILayoutOption[] { GUILayout.Width(40) });
+            updated |= max.Editor();
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.EndVertical();
+
+            EditorGUIUtility.labelWidth = 0;
+
+            return updated;
+        }
+    }
+}
